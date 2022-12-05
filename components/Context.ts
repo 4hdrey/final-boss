e8a0468d5d0d7d89cc0b67862edd866564a91a35
4ndrei <andreygames5@mail.ru>
2022-12-05 21:31:19 +0700

    Update Context.ts

--- components/Context.ts
+++ components/Context.ts
@@ -1,14 +1,11 @@
 import React from "react";
-import { ethers } from "ethers";
 
 const Context = React.createContext<
   [
     boolean,
     React.Dispatch<React.SetStateAction<boolean>>,
     [number, string[], React.CSSProperties[]][],
-    React.Dispatch<
-      React.SetStateAction<[number, string[], React.CSSProperties[]][]>
-    >
+    React.Dispatch<React.SetStateAction<[number, string[], React.CSSProperties[]][]>>
   ]
 >(undefined as any);
 
